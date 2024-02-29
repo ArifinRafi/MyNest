bd15e41fb4b6b3ed68dc37dfffc4a111ab9937fa
ArifinRafi <ahmedrafi364@gmail.com>
2024-02-29 07:17:07 +0600

    footer responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,11 +10,11 @@ import instagram from '../assets/logos/instagram.png';
 const Contact = () => {
     return (
         <div className='relative'>
-            <img className='relative inset-0 w-full h-full object-cover' src={contact} alt="Background" />
-            <div className='absolute mt-24 flex justify-around inset-0 text-white'>
+            <img className='relative inset-0 w-full lg:h-full h-[800px]' src={contact} alt="Background" />
+            <div className='absolute lg:mt-24 flex lg:flex-row flex-col items-center p-4 lg:justify-around inset-0 text-white'>
                 <div><h1 className='text-3xl font-semibold'>Need the best deal?</h1>
                 <h1 className='text-3xl font-semibold'>Contact Us Now!</h1>
-                <div className='flex flex-col gap-2'><p className='text-xl mt-24' >Contact</p>
+                <div className='flex flex-col gap-2'><p className='text-xl lg:mt-24' >Contact</p>
                      <p className='flex gap-4'> <img src={location} alt="" /> House, Road-11, Banani, Dhaka</p>
                      <p className='flex gap-4'> <img src={phone} alt="" />+880142134355</p>
                      <p className='flex gap-4'> <img src={mail} alt="" /> info@mynest.tech</p>
@@ -25,7 +25,7 @@ const Contact = () => {
                           <p>mynest.texh</p>
                      </div>
                      </div></div>
-                <div className='bg-white text-black w-96 gap-4 h-96 flex flex-col items-center'>
+                <div className='bg-white mt-4 text-black w-96 gap-4 h-80 flex flex-col items-center'>
                         <h2 className='text-xl font-bold mt-4'>Contact Now!</h2>
                         <input type="text" placeholder="Your Email" className="input bg-gray-200 text-white input-bordered w-full max-w-xs" />
                         <input type="text" placeholder="Your Query" className="input bg-gray-200 input-bordered w-full max-w-xs" />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
         return (
                 <div className='bg-black flex max-w-screen-2xl mx-auto flex-col justify-center items-center'>
-                        <div className='grid grid-cols-2 h-96 '>
+                        <div className='grid lg:grid-cols-2 grid-cols-1 h-96 '>
                    <div className='flex'>
                    <div className='bg-white flex mt-12 ml-12 justify-center items-center p-2 rounded-full w-24 h-24'>
                         <img src={logo}  className='w-12 h-12' alt="" />
@@ -13,12 +13,12 @@ const Footer = () => {
                         <h2 className='text-white mt-20 ml-4 text-2xl'>MyNest</h2>   
                         </div> 
 
-                        <div className='flex mt-20 text-white gap-6 font-bold justify-center'>
-                                <Link>Terms & Condition</Link>
-                                <Link>Privacy & Policy </Link>
-                                <Link>Blogs</Link>
-                                <Link>About Us</Link>
-                                <Link>Team</Link>
+                        <div className='grid lg:grid-cols-5 lg:grid-cols-1 mt-20  text-white  font-bold justify-items-center'>
+                                <Link className='text-sm w-32'>Terms & Condition</Link>
+                                <Link className='text-sm w-32 ml-4'>Privacy & Policy</Link>
+                                <Link className='text-sm'>Blogs</Link>
+                                <Link className='text-sm'>About Us</Link>
+                                <Link className='text-sm'>Team</Link>
 
                         </div>
                 </div>
