2d75a2e1607c7b5b164e387ee81a24a80fdd085e
ArifinRafi <ahmedrafi364@gmail.com>
2024-03-03 21:40:45 +0600

    propertyData

--- src/pages/PropertyData.jsx
+++ src/pages/PropertyData.jsx
@@ -5,9 +5,38 @@ const PropertyData = () => {
         const properties = useLoaderData();
         return (
                 <div className='flex items-center justify-center'>
-                     <div>{
-                     properties.map(property=> <p key={property._id}>{property.propertyName}</p> )
-                     }</div>   
+                     <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>Property Id</th>
+        <th>Price</th>
+        <th>Address</th>
+        <th>Bedrooms</th>
+        <th>Washrooms</th>
+        <th>Year Built</th>
+        <th>Location</th>
+      </tr>
+    </thead>
+    <tbody>
+      { properties.map(property=> 
+      <tr key={property._id}>
+      <th>{property.propertyName}</th>
+      <td>{property.price}</td>
+      <td>{property.address}</td>
+      <td>{property.bedrooms}</td>
+      <td>{property.washrooms}</td>
+      <td>{property.year}</td>
+      <td>{property.location}</td>
+    </tr> )
+        
+
+      }
+      
+    </tbody>
+  </table>
+</div>
                 </div>
         );
 };
